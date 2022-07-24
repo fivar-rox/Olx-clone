ed3e0bbb3dda5dd46051ab037b5d0e953e977019
fivar-rox <72460532+fivar-rox@users.noreply.github.com>
2022-07-24 18:12:48 +0530

    updated placeholder

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -32,7 +32,7 @@ function Login() {
           <input
             className="input"
             type="email"
-            placeholder="sijeesh@gmail.com"
+            placeholder="something@gmail.com"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
